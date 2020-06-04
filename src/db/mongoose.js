1026acf12b91b830c420391c001fd46a5a2293d7
Omar Ali <omar.moh.aly@gmail.com>
2020-06-04 07:31:15 +0200

    Now connecting to remote testing db

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 const validator = require("validator");
 const uniqueValidator = require("mongoose-unique-validator");
 
-const dbConnectionString = "mongodb://localhost:27017/cookieDBTest";
+// const dbConnectionString = "mongodb://localhost:27017/cookieDBTest";
+const dbConnectionString = "mongodb+srv://omarmohali:spetrivi@cookiecluster-pqhgu.mongodb.net/cookieDBTest?retryWrites=true&w=majority";
 
 mongoose.connect(dbConnectionString, {
     useNewUrlParser: true,
