2b58831dbced5c02c73001b2ec8efa61dc9ca3da
Omar Ali <omar.moh.aly@gmail.com>
2020-06-04 06:43:41 +0200

    Implemented removing comments, will deploy

--- src/routers/comment.js
+++ src/routers/comment.js
@@ -24,16 +24,48 @@ commentRouter.post("/comments", async (req, res) => {
             throw new Error("Missing fields");
         }
 
+        
+
+        const recipe = await Recipe.findById(recipeId);
+
         const comment = Comment({
             commentText: commentText,
             user: user
         })
-
-        const recipe = await Recipe.findById(recipeId);
         var comments = recipe.comments;
         comments.push(comment);
         await recipe.save();
-        res.send(comments);
+        res.send(comment);
+
+    } catch (err) {
+        console.log(err);
+        res.status(400).send()
+    }
+
+});
+
+commentRouter.delete("/comments/:commentId", async (req, res) => {
+    var user;
+    try {
+        user = await User.getUserFromToken(req.headers.authorization);
+    } catch (err) {
+        res.status(401).send(err);
+        return;
+    }
+
+
+
+    try {
+        const commentId = req.params.commentId;
+        const recipeId = req.body.recipeId;
+
+        if (!commentId || !recipeId) {
+            throw new Error("Missing Fields");
+        }
+        
+        await Recipe.update({_id: recipeId}, {$pull: {comments: { _id: commentId }}});
+
+        res.send();
         
     } catch (err) {
         console.log(err);
